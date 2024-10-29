cb7b12eef82283f1bd69dfc0dd6938adbd39b548
Chahine2 <chahinetorino2@gmail.com>
2024-10-29 23:32:37 +0100

    .

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message!');
-    // Here you can add further logic to send the message, e.g., using AJAX.
-});
